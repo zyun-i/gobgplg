fec1f26ed85348038d1b2d21fbb6d91bfc552db4
Isida Zyun'iti <zyun@moin.jp>
2019-06-04 13:00:27 +0000

    fix 2

--- sample.go
+++ sample.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+  "fmt"
   "github.com/gin-gonic/gin"
   "net/http"
+  "os/exec"
 )
 
 func main() {
@@ -10,15 +12,31 @@ func main() {
   router.LoadHTMLGlob("templates/*.tmpl")
 
   router.GET("/", func(c *gin.Context) {
-    c.HTML(http.StatusOK, "index.tmpl", gin.H{ "as": "2222" })
+    c.HTML(http.StatusOK, "index.tmpl", gin.H{ "as": "64565" })
   })
   router.GET("/lg", ping)
   router.Run(":8080")
 }
 
 func ping(c *gin.Context) {
+
+  query := c.Query("query")
+  var cmd string
+
+  switch query {
+  case "0":
+    cmd = "summary"
+  case "1":
+    cmd = "ip bgp"
+  }
+
+  out, err := exec.Command("bgpctl", "show", cmd ).Output()
+  if err != nil {
+	  fmt.Printf("Failed Foo: %s",err)
+  }
+
   c.HTML(http.StatusOK, "index.tmpl", gin.H{
-    "as": "2222",
-    "result": "hoge",
+    "as": "64565",
+    "result": string(out),
   })
 }
